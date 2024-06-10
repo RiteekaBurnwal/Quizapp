dfaacabb77e7c8f377fde913ef62618d6b25afef
To do list task (Bharat  Intern) <sanjeeevkumarpanjiyara98@gmail.com>
2024-06-10 14:24:00 +0530

    this  is my second commit.

--- app/src/main/java/com/practice/quiz_app/QuestionAnswer.java
+++ app/src/main/java/com/practice/quiz_app/QuestionAnswer.java
@@ -1,22 +1,22 @@
 package com.practice.quiz_app;
 public class QuestionAnswer {
     public static String question []={
-            "What is 10+26 ?",
-            "Who invented Telephone?",
+            "What is 10+31 ?",
+            "Who invented computer?",
             "what is 12*9 ?",
             "who is the founder of SpaceX?",
             "In the given options, which is the Example of System Software?"
     };
     public static String choices [][]={
-            {"32" , "42" , "36" , "38"},
-            {"Graham Bell" , "Einstein" , "Edison" , "None of the above"},
+            {"32" , "42" , "41" , "38"},
+            {"Charles Babbage" , "Einstein" , "Edison" , "None of the above"},
             {"96" , "84" , "102" , "108"},
             {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
             {"Windows" , "Linux" , "MacOS" , "All of the above"}
     };
     public static String correctAnswers []={
-            "36" ,
-            "Graham Bell",
+            "41" ,
+            "Charles Babbage",
             "108",
             "Elon Musk",
             "All of the above"};
